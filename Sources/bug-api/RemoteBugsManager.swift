da2fde54727682b5db3567af3af8c8e69245379d
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 01:52:57 +0300

    set integrationToken and databaseID variables, preparing to call the Notion database.

--- Sources/bug-api/RemoteBugsManager.swift
+++ Sources/bug-api/RemoteBugsManager.swift
@@ -9,4 +9,7 @@ import Foundation
 
 public final class RemoteBugsManager {
     
+    private let integrationToken: String = "secret_mVCSP3XNO9XAgvhyfXmL7bYBsM5qxY2uKK0hC1jAPA5"
+    private let databaseID: String = "82b21831560d4631976b0b11e7751bfc"
+    
 }
