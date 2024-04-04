85958e0c3662e3ae0009f653e110d5ea8c81101f
DaniaAlogla <daniaalogla@gmail.com>
2024-04-04 05:29:00 +0300

    create a function that will put image data in the image reference.

--- Sources/bug-api/FirebaseStorageManager.swift
+++ Sources/bug-api/FirebaseStorageManager.swift
@@ -11,7 +11,24 @@ import FirebaseStorage
 class FirebaseStorageManager {
     
     static let shared = FirebaseStorageManager()
-        
+    
     private init() {}
+    
+    // MARK: - Helpers
+    
+    private func putData(in imageRef: StorageReference ,imageData: Data, completion: @escaping (Error?) -> Void){
+        imageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            guard let _ = metadata else {
+                if let error = error {
+                    print("Error in put data proccess: \(error)")
+                    completion(error)
+                } else {
+                    completion(NSError(domain: "com.example.app", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown error"]))
+                }
+                return
+            }
+            completion(nil)
+        }
+    }
     
 }
